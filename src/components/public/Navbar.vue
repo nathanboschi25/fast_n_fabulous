8ffe53dd32ea1e60a4ee22e96fab971bfcea3b7d
Nathan BOSCHI <nathanboschi@outlook.fr>
2023-11-05 18:11:03 +0100

    public navbar creation

---
+++ src/components/public/Navbar.vue
@@ -0,0 +1,79 @@
+<template>
+  <nav class="navbar">
+    <router-link class="logo" to="/">
+      <img alt="Logo" src="@/assets/logo.png">
+    </router-link>
+    <div class="links">
+      <div v-for="(element, id) in links" :key="id" class="link">
+        <router-link :to="element.path">{{ element.name }}</router-link>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "PublicNav",
+  data() {
+    return {
+      links: [
+        {name: "About", path: "/"},
+        {name: "Map", path: "/map"},
+        {name: "Events", path: "/events"},
+      ],
+    };
+  },
+}
+</script>
+
+<style>
+.navbar {
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 50px;
+  padding: 10px 30px;
+  display: flex;
+  justify-content: space-between;
+
+  position: fixed;
+
+  background-color: #000;
+
+  z-index: 1000;
+
+  .logo img {
+    height: 50px;
+    max-width: 15vw;
+  }
+
+  .links {
+    width: 80vw;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    gap: 30px;
+
+    .link {
+      a {
+        color: #fff;
+        text-decoration: none;
+        font-size: 1.2rem;
+        font-weight: 500;
+        padding-bottom: 5px;
+        border-bottom: 3px solid transparent;
+        transition: all 0.2s ease-in-out;
+
+        &.router-link-active {
+          color: yellow;
+          border-bottom: 3px solid yellow;
+        }
+
+        &:hover {
+          border-bottom: 3px solid #6e6e53;
+        }
+      }
+    }
+  }
+}
+</style>
--- src/views/public/Layout.vue
+++ src/views/public/Layout.vue
@@ -1,11 +1,24 @@
 <template>
     <div class="public">
+        <PublicNav />
         <router-view />
     </div>
 </template>
 
 <script>
+import PublicNav from "@/components/public/Navbar.vue";
+
 export default {
     name: "PublicLayout",
+    components: {
+        PublicNav
+    }
 }
 </script>
+
+<style lang="scss">
+html, body {
+    margin: 0;
+    padding: 0;
+}
+</style>
